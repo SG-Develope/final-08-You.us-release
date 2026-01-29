e661c898c35a35373d33df78092e8c0c456220e9
dltmdrb667 <dltmdrb667@naver.com>
2026-01-29 22:40:10 +0900

    feat: 후기 작성및 수정 , 조회페이지 ui 구현

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -4,7 +4,7 @@ export default function ProtectedLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="mx-auto min-h-screen bg-gray-50 lg:max-w-375 lg:min-w-5xl">
+    <div className="mx-auto min-h-screen min-w-90 bg-gray-50 lg:max-w-375">
       {/* AuthGuard, 사용자 정보 표시 */}
       <main>{children}</main>
     </div>
--- src/app/(protected)/mypage/reviews/create/[orderItemId]/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ReviewsPage() {
-  return (
-    <main>
-      <h1>후기 작성</h1>
-      {/* 후기 작성 폼 */}
-    </main>
-  )
-}
